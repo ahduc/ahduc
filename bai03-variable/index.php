116e230728728637d1aedff135afb683cf0a9ca2
ahduc <149230348+ahduc@users.noreply.github.com>
2024-01-12 16:48:26 +0700

    Add files via upload

---
+++ bai03-variable/index.php
@@ -0,0 +1,24 @@
+<?php
+     define('PIA', 53.14);
+     echo PIA;
+?>
+<br>
+
+<?php
+    $str = 'Hello Wolrd';
+    echo $str;
+?>
+<br>
+
+<?php
+     $firstName = 'Nguyen';
+     $lastName = 'Van An';
+     $fullName = $firstName .' '. $lastName;
+     echo $fullName;
+?>
+<br>
+
+<?php
+     $x = 10.365;
+     var_dump($x)
+?>
